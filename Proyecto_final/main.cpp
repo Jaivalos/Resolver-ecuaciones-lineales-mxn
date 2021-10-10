8a0571b2a80a39165b7ffc9c4c4f97bfdc61e799
Javier Avalos <jaivalos18j2@gmail.com>
2021-10-09 18:23:56 -0600

    Funcion para ingresar datos creada

--- Proyecto_final/main.cpp
+++ Proyecto_final/main.cpp
@@ -4,24 +4,25 @@
 using namespace std;
 
 void ingreso();
+void cyf();
 
 double m[100][100];
 int x,y;
 
 int main()
 {
-    ingreso();
+    cyf();
     return 0;
 }
 
 void ingreso(){
 
-cout << "\nResolucion de sistemas de ecuaciones lineales por el metodo de Gauss Jordan \n\nIngrese el numero de columnas: ";
-cin >>x;
-cout << "\nIngrese el numero de Filas: ";
-cin >>y;
+}
 
-system("cls");
-
-
+void cyf(){
+    cout << "\nResolucion de sistemas de ecuaciones lineales por el metodo de Gauss Jordan \n\nIngrese el numero de columnas: ";
+    cin >>x;
+    cout << "\nIngrese el numero de Filas: ";
+    cin >>y;
+    system("cls");
 }
