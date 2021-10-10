9eb449bb8ed22ad8fbbc198f23cd55c8f132437d
Javier Avalos <jaivalos18j2@gmail.com>
2021-10-09 18:19:48 -0600

    Ingreso de columnas y filas anadido

--- Proyecto_final/main.cpp
+++ Proyecto_final/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
+void ingreso();
+
+double m[100][100];
+int x,y;
+
 int main()
 {
+    ingreso();
+    return 0;
+}
+
+void ingreso(){
+
+cout << "\nResolucion de sistemas de ecuaciones lineales por el metodo de Gauss Jordan \n\nIngrese el numero de columnas: ";
+cin >>x;
+cout << "\nIngrese el numero de Filas: ";
+cin >>y;
+
+system("cls");
+
 
 }
